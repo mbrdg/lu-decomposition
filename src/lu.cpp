aa0e41b3652b8a9acfa3b12184d174fa5f5aa092
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-04-29 19:20:51 +0100

    feat: clean redudant unecessary functions

--- src/lu.cpp
+++ src/lu.cpp
@@ -8,12 +8,15 @@
 #include <iostream>
 #include <memory>
 
-template<typename T>
+template<typename T> 
 using matrix_t = T[];
+using matrix_size_t = std::size_t;
+
+static constexpr matrix_size_t matrix_size = 4;
 
 
 template<typename T> 
-void lu(matrix_t<T> A, const std::size_t N)
+void lu(matrix_t<T> A, const matrix_size_t N)
 {
     for (std::size_t k = 0; A[k * N + k] != 0 && k <= N; ++k) {
 
@@ -30,30 +33,19 @@ void lu(matrix_t<T> A, const std::size_t N)
 
 int main(void)
 {
-    constexpr std::size_t matrix_size = 4;
     auto matrix = std::make_unique<matrix_t<double>>(matrix_size * matrix_size);
+    for (std::size_t i = 0; i < matrix_size * matrix_size; ++i) {
+        matrix[i] = 1.0 + static_cast<double>(i);
+    }
 
-    constexpr auto show = [matrix_size]<typename T>(const matrix_t<T> M) {
-        for (std::size_t i = 0; i < matrix_size; ++i) {
-            for (std::size_t j = 0; j < matrix_size; ++j) {
-                std::cout << M[i * matrix_size + j] << '\t';
-            }
-            std::cout << '\n';
+    lu(matrix.get(), matrix_size);
+    
+    for (matrix_size_t i = 0; i < matrix_size; ++i) {
+        for (matrix_size_t j = 0; j < matrix_size; ++j) {
+            std::cout << matrix[i * matrix_size + j] << '\t';
         }
         std::cout << '\n';
-    };
-
-    constexpr auto fill = [matrix_size]<typename T>(matrix_t<T> M) {
-        for (std::size_t i = 0; i < matrix_size * matrix_size; ++i) {
-            M[i] = 1.0 + static_cast<T>(i);
-        }
-    };
-
-    fill(matrix.get());
-
-    show(matrix.get());
-    lu(matrix.get(), matrix_size);
-    show(matrix.get());
+    }
 
     return 0;
 }
